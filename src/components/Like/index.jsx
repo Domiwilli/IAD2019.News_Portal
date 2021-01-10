8018c169e42fba74e65c6d8220273556e94a42c2
Dominique Willi <willidominique@gmail.com>
2021-01-10 21:37:44 +0100

    change like button

--- src/components/Like/index.jsx
+++ src/components/Like/index.jsx
@@ -7,7 +7,7 @@ const Like = ({ start }) => {
   return (
     <div className="like">
       <button className="like__btn" onClick={() => setCount(count + 1)}>
-        {count}&#8194;<img class="iconsmall" src="./assets/images/Icon_like.png"/>
+        {count}&#8194;<img class="iconsmall" src="./assets/images/Icon_like.svg"/>
       </button>
     </div>
   );
